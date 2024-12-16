4b766bdbda611cb1e38dc7f00e654c8196b21f1b
fairofif <fairofif@gmail.com>
2024-12-16 09:15:41 +0700

    add stack screen navigator

---
+++ navigation/AppNavigator.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { createNativeStackNavigator } from '@react-navigation/native-stack'
+import Dashboard from "../screens/Dashboard";
+
+const Stack = createNativeStackNavigator();
+
+export default function AppNavigator() {
+    return (
+        <Stack.Navigator initialRouteName='Dashboard'>
+            <Stack.Screen name='Dashboard' component={Dashboard} options={{headerShown: false}}/>
+        </Stack.Navigator>
+    )
+}
